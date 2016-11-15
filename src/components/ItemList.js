c76d1786f40b0488eaace54b408e78f7894ed661
Matt Stow <matt@mattstow.com>
2016-11-15 17:54:43 +1100

    Dynamic ItemList component

--- src/components/ItemList.js
+++ src/components/ItemList.js
@@ -5,27 +5,32 @@ class ItemList extends Component {
         super();
 
         this.state = {
-            items: [
-                {
-                    id: 1,
-                    label: 'List item 1'
-                },
-                {
-                    id: 2,
-                    label: 'List item 2'
-                },
-                {
-                    id: 3,
-                    label: 'List item 3'
-                },
-                {
-                    id: 4,
-                    label: 'List item 4'
-                }
-            ],
+            items: [],
             hasErrored: false,
             isLoading: false
         };
+    }
+
+    fetchData(url) {
+        this.setState({ isLoading: true });
+
+        fetch(url)
+            .then((response) => {
+                if (!response.ok) {
+                    throw Error(response.statusText);
+                }
+
+                this.setState({ isLoading: false });
+
+                return response;
+            })
+            .then((response) => response.json())
+            .then((items) => this.setState({ items }))
+            .catch(() => this.setState({ hasErrored: true }));
+    }
+
+    componentDidMount() {
+        this.fetchData('http://5826ed963900d612000138bd.mockapi.io/items');
     }
 
     render() {
