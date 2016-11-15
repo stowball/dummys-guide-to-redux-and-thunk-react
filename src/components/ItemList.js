479ce2cfd5ec04b887b670ea95c3488bd5baca0e
Matt Stow <matt@mattstow.com>
2016-11-15 17:54:10 +1100

    Static ItemLIst component

--- src/components/ItemList.js
+++ src/components/ItemList.js
@@ -1,8 +1,51 @@
-import { Component } from 'react';
+import React, { Component } from 'react';
 
 class ItemList extends Component {
+    constructor() {
+        super();
+
+        this.state = {
+            items: [
+                {
+                    id: 1,
+                    label: 'List item 1'
+                },
+                {
+                    id: 2,
+                    label: 'List item 2'
+                },
+                {
+                    id: 3,
+                    label: 'List item 3'
+                },
+                {
+                    id: 4,
+                    label: 'List item 4'
+                }
+            ],
+            hasErrored: false,
+            isLoading: false
+        };
+    }
+
     render() {
-        return null;
+        if (this.state.hasErrored) {
+            return <p>Sorry! There was an error loading the items</p>;
+        }
+
+        if (this.state.isLoading) {
+            return <p>Loading…</p>;
+        }
+
+        return (
+            <ul>
+                {this.state.items.map((item) => (
+                    <li key={item.id}>
+                        {item.label}
+                    </li>
+                ))}
+            </ul>
+        );
     }
 }
 
