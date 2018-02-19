21c547533f39bfffacb218ec289df498a8629338
Justin Sankar Lal <justin.sankarlal@astrazeneca.com>
2018-02-19 12:28:11 +0530

    test

--- server.js
+++ server.js
@@ -1,6 +1,14 @@
 var express = require('express');
 var app = express();
+app.set('port', (process.env.PORT || 7000));
 
-app.use(express.static(__dirname + '/dist/index.html'));
+app.get('/', function (req, res) {
+  res.sendFile(__dirname + '/dist/index.html');
+});
 
-app.listen(process.env.PORT || 8080);
+app.use('/', express.static(__dirname + '/'));
+
+app.listen(app.get('port'), function() {
+  console.log('Node app is running on port', app.get('port'));
+});
+//http://localhost:9000
