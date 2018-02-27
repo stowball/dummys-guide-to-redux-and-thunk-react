586bb1db98eda09f7f70db1788a1b487cf5fb652
Justin Sankarlal <jsankarlal@gmail.com>
2018-02-27 09:44:48 +0530

    FRM build with period charts, Incident Charts and product charts

--- server.js
+++ server.js
@@ -11,7 +11,8 @@ app.get('/login', function (req, res) {
 	var url_parts = url.parse(req.url, true),
 		query = url_parts.query,
 		password = req.query.password,
-		username = req.query.username;
+		username = req.query.username,
+		q = req.query.sql;
 	var conn = new jsforce.Connection({
 	  // you can change loginUrl to connect to sandbox or prerelease env.
 	  loginUrl : req.param('url') || 'https://test.salesforce.com'
@@ -21,7 +22,7 @@ app.get('/login', function (req, res) {
 		response = {};
 	conn.login(username, password, function(err, userInfo) {
 	  if (err) { 
-		res.send(JSON.stringify({message: 'failure'}));
+		res.send(JSON.stringify({message: 'Connection Failure'}));
 		return console.error(err);
 	  }
 	  // Now you can get the access token and instance URL information.
@@ -32,17 +33,31 @@ app.get('/login', function (req, res) {
 	  console.log('conn.instanceUrl' + conn.instanceUrl);
 	  // logged in user property
 	  instanceUrl = conn.instanceUrl;
-	  response = {
+	  connectionString = {
 		accessToken: conn.accessToken,
 		instanceUrl: conn.instanceUrl,
 		userInfo: userInfo,
 		message: 'success'
 	  }
-	  
+	//  res.send(JSON.stringify(connectionString));
 	  console.log("User ID: " + userInfo.id);
 	  console.log("Org ID: " + userInfo.organizationId);
+	  console.log("Query");
+	  
+	conn.query("Select Name, Account_vod__c, Call_Type_AZ_US__c, Status_vod__c, Call_Date_vod__c, CreatedById, LastModifiedById, OwnerId, RecordTypeId from Call2_vod__c", function(err, result) {
+	  console.log("Query Enters");
+	  if (err) { 
+		return console.error(err);
+		res.send(JSON.stringify({message: 'Query Failure'}));
+	  }
+	  console.log(result);
+	  res.send(JSON.stringify(result));
+	  console.log("Query Successful");
+	});
+
+	  
 	  // ...
-	  res.send(JSON.stringify(response));
+	  
 	});
 	//res.send(JSON.stringify(response));
 });
